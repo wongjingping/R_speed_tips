3be37c05582ed623d2aed41b1f8a39b99e752f20
wongjingping <jingpingw@ncs.com.sg>
2016-04-01 09:21:27 +0800

    tidied formatting

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Tips and Tricks for Speeding up R Code
 
-This mini tutorial aims to share some of the practical tips and tricks that have proved useful in shaving off orders of magnitude of processing time for some of my past projects. 
+This mini tutorial aims to share some of the practical tips and tricks that have proved useful in shaving off orders of magnitude of processing time for some of my past projects.
+There's a mini exercise at the end included to help you get some hands-on practice.
+Feel free to submit a pull request if you have better suggestions for the answers!
 
-_Time required_: 30mins
+_Time required_: 30-60mins
 
 _Intended audience_: beginners to intermediate R users
--- speed_training.R
+++ speed_training.R
@@ -6,12 +6,7 @@
 
 # 0) Load Data
 library(nycflights13)
-
-# overview of the data frames we'll be using
-data(airlines)
-data(airports)
 data(flights)
-data(planes)
 
 # 1) Slowness of for loops
 # using for loops with data frames in R is SLOW, as the assignment copies the data frame in each iteration
@@ -19,28 +14,28 @@ data(planes)
 # let's look an example: find the average distance travelled per flight for flights arriving at "BOS"
 # a bad implementation with for loops might look like this:
 get_avg_dist_bos <- function(){
-uniq_flights <- c()
-uniq_dist <- c()
-uniq_count <- c()
-for(i in 1:nrow(flights)){
-if(flights[i,'dest']=='BOS'){
-print(i)
-if(!flights[i,'flight'] %in% uniq_flights){
-uniq_flights <- c(uniq_flights,as.character(flights[i,'flight']))
-uniq_dist <- c(uniq_dist,as.numeric(flights[i,'distance']))
-uniq_count <- c(uniq_count,1)
-} else {
-for(j in 1:length(uniq_flights)){
-if(uniq_flights[j] == flights[i,'flight']){
-uniq_dist[j] <- uniq_dist[j] + flights[i,'distance']
-uniq_count[j] <- uniq_count[j] + 1
-}
-}
-}
-}
-}
-ret <- rbind(uniq_flights,uniq_dist/uniq_count) # ok I cheated here but I'm too lazy
-ret
+    uniq_flights <- c()
+    uniq_dist <- c()
+    uniq_count <- c()
+    for(i in 1:nrow(flights)){
+        if(flights[i,'dest']=='BOS'){
+            print(i)
+            if(!flights[i,'flight'] %in% uniq_flights){
+                uniq_flights <- c(uniq_flights,as.character(flights[i,'flight']))
+                uniq_dist <- c(uniq_dist,as.numeric(flights[i,'distance']))
+                uniq_count <- c(uniq_count,1)
+            } else {
+                for(j in 1:length(uniq_flights)){
+                    if(uniq_flights[j] == flights[i,'flight']){
+                        uniq_dist[j] <- uniq_dist[j] + flights[i,'distance']
+                        uniq_count[j] <- uniq_count[j] + 1
+                    }
+                }
+            }
+        }
+    }
+    ret <- rbind(uniq_flights,uniq_dist/uniq_count) # ok I cheated here but I'm too lazy
+    ret
 }
 
 system.time(result <- get_avg_dist_bos()) # ~200s. Feel free to not run this
@@ -57,9 +52,9 @@ system.time(result <- aggregate(distance ~ flight, flights_bos, mean)) # ~0.037s
 # which has a read-friendly syntax and runs even faster
 library(dplyr)
 system.time(result <- flights %>% 
-filter(dest == 'BOS') %>% 
-group_by(flight) %>% 
-summarise(avg_dist=mean(distance))) # 0.029s!
+                filter(dest == 'BOS') %>% 
+                group_by(flight) %>% 
+                summarise(avg_dist=mean(distance))) # 0.029s!
 # this dataset is not too big and the query is simple so the differences aren't that pronounced
 # but with production scale data, multiple joins and group_by's, dplyr's your best friend
 
@@ -69,7 +64,7 @@ summarise(avg_dist=mean(distance))) # 0.029s!
 # let's find all flights whose tailnum contains any vowel
 # a bad implementation using grepl might look like this:
 system.time(flights_vowel <- flights[grepl('A',flights$tailnum)|grepl('E',flights$tailnum)|grepl('I',flights$tailnum)|
-grepl('O',flights$tailnum)|grepl('U',flights$tailnum),'flight']) # 0.41s
+                                         grepl('O',flights$tailnum)|grepl('U',flights$tailnum),'flight']) # 0.41s
 # we can do better by merging the conditions within the regex pattern:
 system.time(flights_vowel <- flights[grepl('[AEIOU]',flights$tailnum),'flight']) # 0.11s
 # for more help on regex, type ?regex on your R console
@@ -93,46 +88,50 @@ system.time(for(i in 1:nrow(dist_matrix)) avg_dist <- avg_dist + dist_matrix[i,1
 
 # some other options available which are alot more complicated and less worth the time to implement are
 # - naive parallel processing with foreach and %dopar% constructs.
-# - requires installing and setting up some dependencies/configs which might require administrator access
-# - does not work for sequential dependencies in code
-# - bear in mind amdahl's law
+#  - requires installing and setting up some dependencies/configs which might require administrator access
+#  - does not work for sequential dependencies in code
+#  - bear in mind amdahl's law
 # - compiling R code
-# - overhead time requirements when compiling code
-# - no speedup if underlying code is pre-compiled (esp all the basic vectorized functions)
+#  - overhead time requirements when compiling code
+#  - no speedup if underlying code is pre-compiled (esp all the basic vectorized functions)
 # - re-writing slow intensive loops in C/C++
-# - make sure you're not re-inventing the wheel!
+#  - make sure you're not re-inventing the wheel!
 
 # some trade-offs to bear in mind
 # - "Premature optimization is the root of all evil" - Donald Knuth
 # - always do profiling (time, memory) before deciding which section of code to refactor
 # - consider speed-up vs
-# - memory consumption
-# - coding effort
-# - readability
+#  - memory consumption
+#  - coding effort
+#  - readability
 # that said, the earlier tricks introduced generally lead to large speed-ups without giving up any of these
 
 
 ### Practice Time
 
 # using the same flights dataset, write code to answer the following questions:
+
 # 1) Which days in March had more than 700 flights with either a departure or arrival delay?
 
 # 2) Which cities had more than 50% of incoming flights travelling more than 2000(km)?
 
 # 3) Find all flights whose tailnumber either begins with 'N' followed by 5 digits, or ends with 'B'
 
+
+
+
 ### Suggested Answers
 # 1)
 system.time(answer <- flights %>%
-filter(month==3) %>%
-group_by(day) %>%
-summarise(total_delay=sum(ifelse(dep_delay<0|arr_delay<0,1,0),na.rm=T)) %>%
-filter(total_delay > 700))
+                filter(month==3) %>%
+                group_by(day) %>%
+                summarise(total_delay=sum(ifelse(dep_delay<0|arr_delay<0,1,0),na.rm=T)) %>%
+                filter(total_delay > 700))
 # 2)
 system.time(answer <- flights %>% 
-group_by(dest) %>% 
-summarise(prop_long=mean(ifelse(distance>2000,1,0))) %>% 
-filter(prop_long > 0.5)) # 0.096s
+                group_by(dest) %>% 
+                summarise(prop_long=mean(ifelse(distance>2000,1,0))) %>% 
+                filter(prop_long > 0.5)) # 0.096s
 
 # 3)
 system.time(answer <- flights[grepl('([N][0-9]{5})|(B$)',flights$tailnum),'flight'])
